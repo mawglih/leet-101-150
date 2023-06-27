bd1378831461c1190a9e3f2fe23d557a3654cf13
mawglih <mawglih@gmail.com>
2023-06-27 11:14:01 -0400

    LC101

---
+++ exercises_101-150/LC_101_Symmetric_tree/index.js
@@ -0,0 +1,35 @@
+/**
+ * Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if(root == null) return true;
+    const validate = (left, right) => {
+        if(left && right && left.val === right.val) {
+            if(left.next === null && right.next === null) return true;
+            validate(left.next, right.next);
+        } else {
+            return false;
+        }
+    }
+    return validate(root.left, root.right);
+};
